1d72247b3fe34b27375b791f37d9e0cdd475ed4e
khaledkamal <khaledkamal68@gmail.com>
2020-03-23 00:02:30 +0200

    Initial Commit

---
+++ KKPlaceholder/KKPlaceholder.h
@@ -0,0 +1,19 @@
+//
+//  KKPlaceholder.h
+//  KKPlaceholder
+//
+//  Created by Khaled kamal on 3/23/20.
+//  Copyright © 2020 Khaled kamal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KKPlaceholder.
+FOUNDATION_EXPORT double KKPlaceholderVersionNumber;
+
+//! Project version string for KKPlaceholder.
+FOUNDATION_EXPORT const unsigned char KKPlaceholderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KKPlaceholder/PublicHeader.h>
+
+
